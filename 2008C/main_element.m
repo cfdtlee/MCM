62b39a56bbbb1d90f1f7edc28c549dc5623e2ee8
cfdtlee <cfdtlee@gmail.com>
2014-01-15 23:48:32 +0800

    Analyse
    Main element analysation

---
+++ 2008C/main_element.m
@@ -0,0 +1,12 @@
+clc,clear
+load metrics.txt %把原始数据保存在纯文本文件metrics.txt中
+metrics(3:9,:)=1./metrics(3:9,:);
+metrics=metrics';
+metrics_s=zscore(metrics); %数据标准化
+r=corrcoef(metrics_s); %计算相关系数矩阵
+[x,y,z]=pcacov(r); %特征向量,特征值,比重
+f=repmat(sign(sum(x)),size(x,1),1);
+x=x.*f;
+df=metrics_s*x(:,1:4)
+tf=df*z(1:4)/100;
+[stf,ind]=sort(tf,'descend')
